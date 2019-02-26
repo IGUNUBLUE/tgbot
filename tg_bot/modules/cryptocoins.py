a88e44859d634d7ad9a944d661b41645e2815946
IGUNUBLUE <igunublue@protonmail.com>
2019-02-26 11:05:05 -0500

    update module cryptocoins

--- tg_bot/modules/cryptocoins.py
+++ tg_bot/modules/cryptocoins.py
@@ -12,18 +12,20 @@ from tg_bot import dispatcher
 @run_async
 #@user_admin
 def price(bot: Bot, update: Update):
-    msg = update.effective_message  # type: Optional[Message]
+    msg = update.effective_message
+    chat = update.effective_chat 
     #if __name__ == "__crypto__":
-    url = "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        bpi = data['bpi']
-        #for key, value in bpi.items():
-        coin = round(bpi['USD']['rate_float'], 2)#, bpi[key]['rate']
-        msg.reply_text("📉 *BTC price:*\n💲`"+str(coin)+" USD`\n _By coindesk._", parse_mode='MARKDOWN')
-    else:
-        msg.reply_text("*El API no esta operativa*", parse_mode='MARKDOWN')
+    if chat.type != chat.PRIVATE:
+        url = "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            bpi = data['bpi']
+            #for key, value in bpi.items():
+            coin = round(bpi['USD']['rate_float'], 2)#, bpi[key]['rate']
+            msg.reply_text("📉 *BTC price:*\n💲`"+str(coin)+" USD`\n _By coindesk._", parse_mode='MARKDOWN')
+        else:
+            msg.reply_text("*El API no esta operativa*", parse_mode='MARKDOWN')
 
 __mod_name__ = "Cryptocoins"
 
